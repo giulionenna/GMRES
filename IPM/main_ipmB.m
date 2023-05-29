f81e4bba61a32605f335ea54a2616684890c769c
giulio524 <62382801+giulionenna@users.noreply.github.com>
2023-05-29 18:32:01 +0200

    finita trattazione ipm

--- IPM/main_ipmB.m
+++ IPM/main_ipmB.m
@@ -4,11 +4,29 @@ clc
 
 format short
 
-fprintf("--------------Problem Initialization-----------\n");
+ls_method_vec ={'augmented', 'substitution'};
+gmres_mod_vec = {'v1', 'native'};
+K_vec = [100, 500];
+n_vec = [3, 4, 5];
+fid = fopen('log.txt', 'w');
 
-% Initialization of the problem dimensions
-n = 10^4;
-K = 200;
+for i = [1:2]
+    for j = [1:2]
+        for k_iter=[1:2]
+            for h = [1:3]
+fprintf(fid, "--------------Problem Initialization-----------\n");
+fprintf(fid,"Running with params:\n");
+fprintf(fid,"\t ls_method: \t %s \n", ls_method_vec{i});
+ls_method = ls_method_vec{i};
+fprintf(fid,"\t gmres_mod: \t %s \n", gmres_mod_vec{j});
+gmres_mod = gmres_mod_vec{j};
+fprintf(fid,"\t K: \t\t\t %i \n", K_vec(k_iter));
+K = K_vec(k_iter);
+fprintf(fid,"\t n: \t\t\t 10^%i \n", n_vec(h));
+n = 10^n_vec(h);
+
+
+
 
 % Definition of the non-convex quadratic objective function
 c = ones(n,1);
@@ -42,16 +60,15 @@ y0 = ones(2*K+n,1); % y = Ax-b
 lambda0 = ones(2*K+n,1);
 x0 = A\(b+y0); % <- least squares sense
 
-ls_method = 'augmented';
-%ls_method = 'substitution';
 
-gmres_mod = ['v1'];
 
-fprintf("--------------Start IPM Predictor Corrector----\n");
+fprintf(fid,"--------------Start IPM Predictor Corrector----\n");
 % Predictor-Corrector Interior Point Method
+tic
 [xk, fk, yk, lambdak, muk, k, fkseq, tau_seq] = predictor_corrector_ipm_qp_B(...
     Q, c, A, b, tau, eps, kmax, x0, y0, lambda0, ls_method, gmres_mod);
-fprintf("--------------End IPM Predictor Corrector------\n");
+fprintf(fid,"--------------End IPM Predictor Corrector------\n");
+fprintf(fid,"Time elapsed\t\t\t\t\t\t=\t%.3e sec \n", toc);
 %A*xk
 
 % Error check of the KKT conditions
@@ -67,17 +84,22 @@ kkt3_max_err = max(yk.*lambdak);
 kkt4_ineq_x = all(yk < 0);
 kkt4_ineq_s = any(lambdak < 0);
 
-fprintf("--------------KKT condtion errors--------------\n");
-fprintf(" Stationarity (2)\t\t\t\t\t=\t%.3e\n", kkt1_err_linf);
-fprintf(" Stationarity (inf)\t\t\t\t\t=\t%.3e\n", kkt1_err_l2);
-fprintf(" Primal Feasibility Equality (2)\t=\t%.3e\n", kkt2_err_l2);
-fprintf(" Primal Feasibility Equality (inf)\t=\t%.3e\n", kkt2_err_linf);
-fprintf(" Primal Feasibility Inequality\t\t=\t%d\n", kkt4_ineq_x);
-fprintf(" Dual Feasibility Inequality\t\t=\t%d\n", kkt4_ineq_s);
-fprintf(" Complementary Slackness (max)\t\t=\t%.3e\n", kkt3_max_err);
-fprintf(" Complementary Slackness (prd)\t\t=\t%.3e\n", kkt3_prod);
+fprintf(fid,"--------------KKT condtion errors--------------\n");
+fprintf(fid," Stationarity (2)\t\t\t\t\t=\t%.3e\n", kkt1_err_linf);
+fprintf(fid," Stationarity (inf)\t\t\t\t\t=\t%.3e\n", kkt1_err_l2);
+fprintf(fid," Primal Feasibility Equality (2)\t=\t%.3e\n", kkt2_err_l2);
+fprintf(fid," Primal Feasibility Equality (inf)\t=\t%.3e\n", kkt2_err_linf);
+fprintf(fid," Primal Feasibility Inequality\t\t=\t%d\n", kkt4_ineq_x);
+fprintf(fid," Dual Feasibility Inequality\t\t=\t%d\n", kkt4_ineq_s);
+fprintf(fid, " Complementary Slackness (max)\t\t=\t%.3e\n", kkt3_max_err);
+fprintf(fid," Complementary Slackness (prd)\t\t=\t%.3e\n\n\n\n", kkt3_prod);
 
-save("ipm_qp_results.mat")
+%save("ipm_qp_results.mat")
+            end
+        end
+    end
+end
+fclose(fid)
 
 
 
